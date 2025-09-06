cf0b0970ef4dee4c432f3c4c33855283832031d8
kaneki7379 <singh.yash9919@gmail.com>
2025-09-07 01:46:32 +0530

    05-dashboard-part -1

---
+++ components/ui/empty-state.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Image from "next/image";
+
+interface Props {
+  title: string;
+  description: string;
+  imageSrc?: string;
+}
+
+const EmptyState = ({ title, description, imageSrc }: Props) => {
+  return (
+    <div className="flex flex-col items-center justify-center py-16">
+      <Image
+        src={imageSrc!}
+        alt={title}
+        width={192} // w-48 = 192px
+        height={192} // h-48 = 192px
+        className="mb-4"
+      />
+      <h2 className="text-xl font-semibold text-gray-500">{title}</h2>
+      <p className="text-gray-400">{description}</p>
+    </div>
+  );
+};
+
+export default EmptyState;
